a996f3b55aff27ee65f868a025160ab22f664fd6
Kade Angell <ponmlk523@gmail.com>
2024-04-20 06:27:16 -0600

    Finished backend

--- backend/chatGPT.ts
+++ backend/chatGPT.ts
@@ -1,16 +1,42 @@
 import OpenAI from 'openai'
+import dotenv from 'dotenv'
+import prompts from './prompts'
+
+dotenv.config()
 
 export const requestGPTResponse = async (message: string) => {
     const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 
-    const completion = await openai.chat.completions.create({
-        messages: [{"role": "system", "content": "You are attempting to blend in with humans responding to this prompt. Try to answer in a way that a human would, trying to sound like a bot. Use a short response. Do not provide an explanation"},
-            {"role": "user", "content": message}],
-        model: "gpt-4-turbo",
-        temperature: 0.9,
-    });
-    
-    console.log(completion.choices[0]);
+    const tones = [
+        'Sound like a 6-year old girl',
+        'Sound excited!',
+        'Sound like a robot',
+        'Sound bored'
+    ]
 
-    return completion.choices[0].message.content;
+    const tone = tones[Math.floor(Math.random() * tones.length)]
+
+    const completion = await openai.chat.completions.create({
+        messages: [
+            {
+                role: 'system',
+                content:
+                    'You are attempting to blend in with humans responding to this prompt. \
+                    Try to answer in a way that a human would. \
+                    Use a short response. Do not provide an explanation.' + tone
+            },
+            { role: 'user', content: message }
+        ],
+        model: 'gpt-4-turbo',
+        temperature: 0.8
+    })
+
+    // console.log(completion.choices[0])
+    console.log('Tone: ' + tone)
+
+    return completion.choices[0].message.content
 }
+
+const prompt = prompts[Math.floor(Math.random() * prompts.length)]
+console.log(prompt)
+requestGPTResponse(prompt).then((response) => console.log(response))
--- backend/websocket.ts
+++ backend/websocket.ts
@@ -1,39 +1,21 @@
 import { IncomingMessage, Server, ServerResponse } from 'http'
 import { Server as SocketIOServer } from 'socket.io'
-import prompts from './prompts';
-import { requestGPTResponse } from './chatGPT';
-
-interface PlayerMap {
-    [key: string]: string
-}
-
-interface RoomMap {
-    [key: string]: { displayID: string; players: PlayerMap, usedPrompts: number[], responses: responseVotes }
-}
-
-interface joinRoomPayload {
-    name: string
-    code: string
-}
-
-interface responseVotes {
-    [key: string]: number
-}
-
-interface joinRoomValidation {
-    validCode: boolean
-    validName: boolean
-    host: boolean
-}
-
-interface displayPlayerJoin {
-    name: string
-    host: boolean
-}
+import prompts from './prompts'
+import { requestGPTResponse } from './chatGPT'
+import {
+    RoomMap,
+    displayPlayerJoin,
+    displayPromptResponse,
+    joinRoomPayload,
+    joinRoomValidation
+} from './types/websockets'
 
 const setupWebSocket = (server: Server<typeof IncomingMessage, typeof ServerResponse>) => {
     const io = new SocketIOServer(server)
     const roomHosts: RoomMap = {}
+    const numRounds = 4
+    let roundNum = 0
+    let numVotes = 0
 
     // Generate a random prompt from the list
     const generatePrompt = (roomCode: string) => {
@@ -42,7 +24,7 @@ const setupWebSocket = (server: Server<typeof IncomingMessage, typeof ServerResp
             index = Math.floor(Math.random() * prompts.length)
         }
         roomHosts[roomCode].usedPrompts.push(index)
-    
+
         return prompts[index]
     }
 
@@ -54,10 +36,14 @@ const setupWebSocket = (server: Server<typeof IncomingMessage, typeof ServerResp
             while (roomCode in roomHosts) {
                 roomCode = Math.random().toString(36).toUpperCase().slice(2, 6)
             }
-            console.log(roomCode)
 
             socket.join(roomCode)
-            roomHosts[roomCode] = { displayID: socket.id, players: {}, usedPrompts: [], responses: {} } // Store the host's socket ID
+            roomHosts[roomCode] = {
+                displayID: socket.id,
+                players: {},
+                usedPrompts: [],
+                responses: {}
+            } // Store the host's socket ID
 
             io.to(roomCode).emit('display_code', { code: roomCode })
 
@@ -66,12 +52,22 @@ const setupWebSocket = (server: Server<typeof IncomingMessage, typeof ServerResp
 
         socket.on('join_room', (payload: joinRoomPayload) => {
             socket.join(payload.code)
-            roomHosts[payload.code].players[socket.id] = payload.name
+            roomHosts[payload.code].players[socket.id] = {
+                name: payload.name,
+                id: socket.id,
+                host: false,
+                responses: [],
+                points: 0
+            }
 
             const responsePayload: joinRoomValidation = {
                 validCode: payload.code in roomHosts,
                 validName: payload.name !== '',
                 host: Object.keys(roomHosts[payload.code].players).length === 1 // If there is only one player in the room, they are the host
+            }
+
+            if (responsePayload.host) {
+                roomHosts[payload.code].players[socket.id].host = true
             }
 
             io.to(socket.id).emit('join_room_validation', responsePayload)
@@ -82,45 +78,102 @@ const setupWebSocket = (server: Server<typeof IncomingMessage, typeof ServerResp
                 }
 
                 io.to(roomHosts[payload.code].displayID).emit('display_player_join', displayPayload)
-                
+
                 console.log(`Player ${socket.id} joined room: ${payload.code}`)
             }
-
         })
 
-
-        socket.on('host_start_game', (roomCode: string) => {
+        socket.on('host_start_game', async (roomCode: string) => {
             io.to(roomCode).emit('game_started')
             const prompt = generatePrompt(roomCode)
             io.to(roomCode).emit('prompt', prompt)
-            const gptResponse = requestGPTResponse(prompt)
-            setTimeout(() => {
-                io.to(roomCode).emit('display_reveal_responses', Object.keys(roomHosts[roomCode].responses))
-                io.to(roomCode).emit('player_reveal_responses', Object.keys(roomHosts[roomCode].responses))
+            const gptResponse = await requestGPTResponse(prompt)
+            if (gptResponse) {
+                roomHosts[roomCode].responses[gptResponse] = {
+                    round: roundNum,
+                    votes: 0,
+                    player_id: '0',
+                    isBot: true
+                }
+            }
+
+            if (roundNum < numRounds) {
+                io.to(roomCode).emit('start_timer', { duration: 60 })
                 setTimeout(() => {
-                    io.to(roomCode).emit('display_votes', { responses: roomHosts[roomCode].responses })
+                    io.to(roomCode).emit('display_reveal_responses', { bot_response: gptResponse })
+                    io.to(roomCode).emit(
+                        'player_reveal_responses',
+                        Object.keys(roomHosts[roomCode].responses)
+                    )
+
+                    io.to(roomCode).emit('start_timer', { duration: 60 })
+                    setTimeout(() => {
+                        io.to(roomCode).emit('display_votes', {
+                            responses: roomHosts[roomCode].responses
+                        })
+                    }, 60000)
                 }, 60000)
-            }, 60000)
+                roundNum++
+            } else {
+                let playerScores: { player: string; points: number }[] = []
+
+                for (const playerKey in roomHosts[roomCode].players) {
+                    const player = roomHosts[roomCode].players[playerKey]
+                    playerScores.push({ player: player.name, points: player.points })
+
+                    // Send the final scores to the players
+                    io.to(player.id).emit('player_final_score', {
+                        points: player.points
+                    })
+                }
+
+                io.to(roomCode).emit('display_final_scores', {
+                    players: playerScores
+                })
+            }
+        })
+
+        socket.on('player_prompt_response', (response: string) => {
+            const roomCode = socket.rooms.values().next().value
+            roomHosts[roomCode].players[socket.id].responses.push(response)
+            roomHosts[roomCode].responses[response] = {
+                round: roundNum,
+                votes: 0,
+                player_id: socket.id,
+                isBot: false
+            }
+            socket.emit('display_prompt_response', {
+                player: roomHosts[roomCode].players[socket.id].name,
+                response
+            } as displayPromptResponse)
         })
 
         socket.on('player_submit_vote', (response: string) => {
-            // TODO
-        })
+            const roomCode = socket.rooms.values().next().value
 
-        socket.on('player_prompt_response', (payload: any) => {
+            roomHosts[roomCode].responses[response].votes++
+            if (roomHosts[roomCode].responses[response].isBot) {
+                roomHosts[roomCode].players[socket.id].points += 200
+            } else {
+                roomHosts[roomCode].players[
+                    roomHosts[roomCode].responses[response].player_id
+                ].points += 100
+            }
+            if (numVotes === Object.keys(roomHosts[roomCode].players).length) {
+                const responsesObject = roomHosts[roomCode].responses
+                const responsesArray = Object.keys(responsesObject).map((key) => ({
+                    response: key,
+                    votes: responsesObject[key].votes
+                }))
 
-            socket.emit('display_prompt_response', {
-                ...payload,
-                name: roomHosts[payload.code].players[socket.id]
-            })
-        })
-
-        socket.on('display_reveal_responses', (payload: any) => {
-            // TODO
-        })
-
-        socket.on('player_reveal_responses', (payload: any) => {
-            // TODO
+                socket.emit('display_votes', { responses: responsesArray })
+                for (const playerID in Object.keys(roomHosts[roomCode].players)) {
+                    io.to(playerID).emit('player_points', {
+                        player: roomHosts[roomCode].players[playerID].name,
+                        points: roomHosts[roomCode].players[playerID].points
+                    })
+                }
+            }
         })
 
         socket.on('disconnect', () => {
